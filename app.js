74189446a1548f4ccc9128199370cb0968b649a9
taiyebnirjhar <nirjhar.212902003.green.ac.bd>
2022-03-05 23:44:44 +0600

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+const countdown = () => {
+    const countDate = new Date('April 30, 2022 23: 59: 59').getTime();
+    const now = new Date().getTime();
+
+    const gap = countDate - now;
+
+    // how the fuck time work?
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // calculate shit
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.day').innerHTML = textDay;
+    document.querySelector('.hour').innerHTML = textHour;
+    document.querySelector('.minute').innerHTML = textMinute;
+    document.querySelector('.second').innerHTML = textSecond;
+
+
+
+    if (gap <= 604800000) // 7 days
+    {
+        document.querySelector('#image').src = 'times.jpg';
+
+    }
+    if (gap <= 0) {
+        document.querySelector('#hd').innerHTML = "Did You Get The Fucking Job";
+        document.querySelector('#image').src = 'timeOut.png';
+
+    }
+}
+countdown();
+setInterval(countdown, 1000);
